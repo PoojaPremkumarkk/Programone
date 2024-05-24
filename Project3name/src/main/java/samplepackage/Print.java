4150355ddad862fbd34e8bbe0e7ec2920258356f
pooja <pooja@LAPTOP-FD43FT9U>
2024-05-24 20:40:43 +0500

    Today

---
+++ Project3name/src/main/java/samplepackage/Print.java
@@ -0,0 +1,14 @@
+package samplepackage;
+
+public class Print {
+	public void print()
+	{
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		Print p=new Print();
+		p.print();
+	}
+
+}
